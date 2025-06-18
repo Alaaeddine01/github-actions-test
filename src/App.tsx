7307858f1ea6dfce3c3892c54434946b33bc30b9
Alaaeddine elhaouch <alaaeddine958@gmail.com>
2025-06-18 16:36:59 +0100

    modification sur code

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Projet Alaaeddine</h1>
+      <h1>Projet Alaaeddine modified</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
